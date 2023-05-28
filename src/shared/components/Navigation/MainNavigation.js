2cde18abd2ec07cb6ac7833ddcb3e86a6d271a52
lonelydomino <lonelydomino@icloud.com>
2023-05-28 12:23:43 -0700

    Adding side drawer.

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,23 +2,32 @@ import React from 'react'
 
 import './MainNavigation.css'
 import MainHeader from './MainHeader'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { Link } from 'react-router-dom'
+import NavLinks from './NavLinks'
+import SideDrawer from './SideDrawer'
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-        <button className='main-navigation__menu-btn'>
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className='main-navigation__title'>
-            <Link to='/'>Your Places</Link>
-        </h1>
-        <nav>
-            ...
-        </nav>
-    </MainHeader>
+    <>
+        <SideDrawer>
+            <nav className='main-navigation__drawer-nav'>
+                <NavLinks />
+            </nav>
+        </SideDrawer>
+        <MainHeader>
+            <button className='main-navigation__menu-btn'>
+                <span />
+                <span />
+                <span />
+            </button>
+            <h1 className='main-navigation__title'>
+                <Link to='/'>Your Places</Link>
+            </h1>
+            <nav className='main-navigation__header-nav'>
+                <NavLinks />
+            </nav>
+        </MainHeader>
+    </>
   )
 }
 
