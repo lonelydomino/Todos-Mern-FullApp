c7ca096e02f815a8b8c1f446a9d46c5789bca60c
lonelydomino <lonelydomino@icloud.com>
2023-06-08 20:43:42 -0700

    Adding validators

--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,14 +1,76 @@
-import React from 'react'
-import './Input.css'
-const Input = (props) => {
-  const element = props.element === 'input' ? (<input id={props.id} type={props.type} placeholder={props.placeholder} /> ): ( <textarea id={props.id} rows={props.rows || 3} />)
-  
-    return (
-    <div className={`form-control `}>
-        <label htmlFor={props.id}>{props.label}</label>
-        {element}
-    </div>
-  )
-}
+import React, { useReducer } from "react";
+import "./Input.css";
+import { validate } from "../../util/validators";
 
-export default Input
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.payload,
+        isValid: validate(action.payload, action.validators),
+      };
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: "",
+    isValid: false,
+    isTouched: false,
+  });
+
+  const changeHandler = (e) => {
+    dispatch({
+      type: "CHANGE",
+      payload: e.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({ type: "TOUCH" });
+  };
+
+  const element =
+    props.element === "input" ? (
+      <input
+        id={props.id}
+        type={props.type}
+        placeholder={props.placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && "form-control--invalid"
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+      {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
