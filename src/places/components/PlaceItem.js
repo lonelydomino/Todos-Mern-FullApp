eba62550d67fbec1ee6eb0a5c807ca139453a7fd
lonelydomino <lonelydomino@icloud.com>
2023-06-12 17:56:58 -0700

    Finishing edit page

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react'
-import Card from '../../shared/components/UIElements/Card'
-import Button from '../../shared/components/FormElements/Button'
-import './PlaceItem.css'
-import Modal from '../../shared/components/UIElements/Modal'
-import Map from '../../shared/components/UIElements/Map'
+import React, { useState } from "react";
+import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
+import "./PlaceItem.css";
+import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 const PlaceItem = (props) => {
-  const [showMap, setShowMap] = useState(false)
+  const [showMap, setShowMap] = useState(false);
 
+  const [showConfirmModal, setShowConfirmModal] = useState(false)
   const openMapHandler = () => {
-    setShowMap(true)
-  }
+    setShowMap(true);
+  };
   const closeMapHandler = () => {
-    setShowMap(false)
+    setShowMap(false);
+  }
+
+  const showDeleteWarningHandler = () => {
+    setShowConfirmModal(true)
+  }
+  const cancelDeleteHandler = () => {
+    setShowConfirmModal(false)
+  }
+  const confirmDeleteHandler = () => {
+    setShowConfirmModal(false)
+    console.log('DELETING...')
   }
   return (
     <>
@@ -23,12 +35,28 @@ const PlaceItem = (props) => {
         footerClass="place-item__modal-actions"
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
-        <div className="map-container"><Map center={props.coordinates} zoom={16} /></div>
+        <div className="map-container">
+          <Map center={props.coordinates} zoom={16} />
+        </div>
+      </Modal>
+      <Modal
+        show={showConfirmModal}
+        onCancel={cancelDeleteHandler}
+        header="Are you sure?"
+        footerClass="place-item__modal-actions"
+        footer={
+          <>
+            <Button inverse onClick={cancelDeleteHandler}>CANCEL</Button>
+            <Button danger onClick={confirmDeleteHandler}>DELETE</Button>
+          </>
+        }
+      >
+        <p>Do you want to proceed? This action cannot be undone.</p>
       </Modal>
       <li>
         <Card className="place-item__content">
           <div className="place-item__image">
-            <img src={props.image} alt={props.title} />{' '}
+            <img src={props.image} alt={props.title} />{" "}
           </div>
           <div className="place-item__info">
             <h2>{props.title}</h2>
@@ -36,14 +64,16 @@ const PlaceItem = (props) => {
             <p>{props.description}</p>
           </div>
           <div className="place-item__actions">
-            <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
             <Button to={`/places/${props.id}`}>EDIT</Button>
-            <Button danger>DELETE</Button>
+            <Button  onClick={showDeleteWarningHandler} danger>DELETE</Button>
           </div>
         </Card>
       </li>
     </>
-  )
-}
+  );
+};
 
-export default PlaceItem
+export default PlaceItem;
