d19174ebdcc3038ec25cd9d46d02800b36dd68a4
lonelydomino <lonelydomino@icloud.com>
2023-10-31 09:22:18 -0700

    first commit

--- api/server.js
+++ api/server.js
@@ -32,6 +32,7 @@ app.post('/todo/new', (req, res) => {
     res.json(todo)
 })
 
+
 app.delete('/todo/delete/:id', async (req, res) => {
     console.log("IN DELETE")
     const result = await Todo.findByIdAndDelete(req.params.id)
@@ -46,4 +47,4 @@ app.get('/todo/complete/:id', async (req, res) => {
     todo.save()
     res.json(todo)
 })
-app.listen(3001, () => console.log("Server started on port 3001."))
+app.listen(process.env.PORT || 3001, '0.0.0.0', () => console.log("Server started on port 3001."))
